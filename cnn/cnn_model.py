0ddf8b4d1e2a3b20b4c9c6df237c2458a382106d
ankkitt <ankittcse@gmail.com>
2026-02-12 20:50:18 +0530

    Implement 2-layer custom CNN forward pass with multi-filter support

--- cnn/cnn_model.py
+++ cnn/cnn_model.py
@@ -4,18 +4,77 @@ from cnn.conv import convolve
 from cnn.activation import relu
 from cnn.pooling import max_pooling
 
+
 class CustomCNN:
 
     def __init__(self):
-        self.kernel1 = np.array([
-            [-1, 0, 1],
-            [-2, 0, 2],
-            [-1, 0, 1]
-        ])
+
+        # -------- Layer 1 Kernels --------
+        self.layer1_kernels = [
+            np.array([
+                [-1, 0, 1],
+                [-2, 0, 2],
+                [-1, 0, 1]
+            ]),  # Horizontal edge
+
+            np.array([
+                [-1, -2, -1],
+                [ 0,  0,  0],
+                [ 1,  2,  1]
+            ])   # Vertical edge
+        ]
+
+        # -------- Layer 2 Kernels --------
+        self.layer2_kernels = [
+            np.array([
+                [1, 1, 1],
+                [1, 1, 1],
+                [1, 1, 1]
+            ]),  # Aggregation filter
+
+            np.array([
+                [0, -1, 0],
+                [-1, 4, -1],
+                [0, -1, 0]
+            ])   # Structural emphasis
+        ]
+
+
+    def apply_filters(self, image, kernels):
+        outputs = []
+        for kernel in kernels:
+            feature = convolve(image, kernel)
+            outputs.append(feature)
+        
+        return outputs
+
 
     def forward(self, image):
-        conv1 = convolve(image, self.kernel1)
-        relu1 = relu(conv1)
-        pool1 = max_pooling(relu1)
 
-        return pool1
+        # ---------- Layer 1 ----------
+        feature_maps1 = self.apply_filters(image, self.layer1_kernels)
+
+        activated1 = []
+        for fm in feature_maps1:
+            activated1.append(relu(fm))
+
+        pooled1 = []
+        for fm in activated1:
+            pooled1.append(max_pooling(fm))
+
+
+        # ---------- Layer 2 ----------
+        feature_maps2 = []
+        for fm in pooled1:
+            feature_maps2.extend(self.apply_filters(fm, self.layer2_kernels))
+
+        activated2 = []
+        for fm in feature_maps2:
+            activated2.append(relu(fm))
+
+        pooled2 = []
+        for fm in activated2:
+            pooled2.append(max_pooling(fm))
+
+
+        return pooled2
